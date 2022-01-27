b613c1bab7f964f90c14b93b710ae188ca2908e7
Duan <15034966931@163.com>
2022-01-27 10:57:51 +0800

    删除冗余内容

--- Assets/Scripts/_HotUpdateFW/IStartGame.cs
+++ Assets/Scripts/_HotUpdateFW/IStartGame.cs
@@ -1,16 +1,8 @@
-﻿/***
- *
- *  Title: "热更新流程设计"课程项目
- *        
- *          接口： 游戏开始接口
- *          
- *
+﻿/***     
+ *  游戏开始接口
  *  Description:
- *        功能：
- *             与"从服务器下载更新最新的资源文件"（UpdateResourcesFileFromServer）脚本配合，
- *             进行自动化数值传递，特指定本接口。
- *
- *     
+ *     与"从服务器下载更新最新的资源文件"（UpdateResourcesFileFromServer）脚本配合，
+ *     进行自动化数值传递，特指定本接口。  
  */
 
 using System;
